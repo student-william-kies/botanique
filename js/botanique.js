ff1452e9133d18757d260801f413b398b8d41a45
WylsTix <william.kies04@gmail.com>
2020-10-21 16:37:37 +0200

    Fin projet

---
+++ js/botanique.js
@@ -0,0 +1,17 @@
+// FUNCTION POUR LES RESEAUX
+
+function fonctionFacebook(){
+  window.open("https://www.facebook.com/guillaume.grac","_blank");
+}
+
+function fonctionTwitter(){
+  window.open("https://twitter.com/Mac_Millou","_blank");
+}
+
+function fonctionInstagram(){
+  window.open("https://www.instagram.com/gui.grc/","_blank");
+}
+
+function fonctionGit(){
+  window.open("https://github.com/guillaume-grac","_blank");
+}
